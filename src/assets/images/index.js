6935acb1232fe52c0c6dc0dbbc1a900a7d9d550f
shantmkhitaryan1 <mkhitaryanshant2@gmail.com>
2024-10-28 15:51:53 +0400

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,25 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+
+import cyber from './cyber.png'
+import mes from './mes.png'
+import shengavit from './shengavit.png'
+import iguan from './iguan.png'
+
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    cyber, 
+    mes, 
+    shengavit, 
+    iguan,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,230 @@
+import {  mes, iguan, cyber, shengavit } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React Developer",
+        company_name: "Iguan Systems LLC",
+        icon: iguan,
+        iconBg: "#accbe1",
+        date: "Feb 2024 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+           
+        ],
+    },
+    {
+        title: "React Developer",
+        company_name: "Cyber-Tech Armenia",
+        icon: cyber,
+        iconBg: "#fbc3bc",
+        date: "April 2022 - 2023",
+        points: [
+            "Responsible for the implementation of user interfaces (UIs) and UI components using React, a front-end JavaScript library.", 
+            "Experience in building ecommerce websites, landing pages, admin panels.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+        ],
+    },
+    {
+        title: "Instructor",
+        company_name: "MES of RA",
+        icon: mes,
+        iconBg: "#b7e4c7",
+        date: "2013 - 2021",
+        points: [            
+            "Center for Crisis Management"
+            
+        ],
+    },
+    {
+        title: "Economist",
+        company_name: "Shengavit Medical Centre",
+        icon: shengavit,
+        iconBg: "#a2d2ff",
+        date: "2009 - 2011",
+        points: [
+       
+        ],
+    },
+    {
+        title: "Teacher",
+        company_name: "Anania Shirakaci Acadamy",
+        icon: null,
+        iconBg: "#a2d2ff",
+        date: "2008 - 2009",
+        points: [
+            "Teacher of Mathamatics in English language."
+        ],
+    },
+
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Naira777',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/naira-sahakyan-1338551b4/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-blue',
+        name: 'Manufacturing equipments',
+        description: 'Web application about manufacturer of various types of equipments. With backend and responsive design.',
+        link: 'https://vehicle-app-5mof.onrender.com/home',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Watermark',
+        description: 'Watermark application with responsive design',
+        link: 'https://naira777.github.io/Watermark-1',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-red',
+        name: 'Country Code',
+        description: 'Application with basic information for each country',
+        link: 'https://naira777.github.io/Country_Code',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Realtime chat',
+        description: 'Realtime chat',
+        link: 'https://naira777.github.io/react-chat-app/#',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Application about weather, horoscope signs and astronomy.',
+        description: 'Application about weather, horoscope signs and astronomy.',
+        link: 'https://naira777.github.io/My-Project/#/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'E-commerce',
+        description: 'E-commerce website with firebase',
+        link: 'https://ecommerce-website-64bfa.web.app/cart',
+    }
+];
